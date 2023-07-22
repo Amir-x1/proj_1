4c70602d3ac044ff4e0c117ae85ab3f5162f27f7
Amir-Dell <zendehboodi.amir@gmail.com>
2023-07-22 12:19:56 +0330

    data is dynamic but not from database ready to connect to db

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Rooms:
+    id: int
+    name: str
+    room: int
+    img: str
+    price: int
+    wifi: bool
--- hotel/views.py
+++ hotel/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from .models import Rooms
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    room1 = Rooms()
+    room1.id = 1
+    room1.room = 2
+    room1.img = 'room-1.jpg'
+    room1.name = 'Dubi'
+    room1.price = 103
+    room1.wifi = False
+
+    room2 = Rooms()
+    room2.id = 1
+    room2.room = 22
+    room2.img = 'room-2.jpg'
+    room2.name = 'Bushehr'
+    room2.price = 250
+    room2.wifi = True
+
+    room3 = Rooms()
+    room3.id = 1
+    room3.room = 33
+    room3.img = 'room-3.jpg'
+    room3.name = 'Talhe'
+    room3.price = 123
+    room3.wifi = False
+
+    rooms = [room1, room2, room3]
+
+    return render(request, 'index.html', {'rooms': rooms})
